346b1568f967b4a384c504531b6027c7819005b9
kuqmua <serezhashmakov@outlook.com>
2022-03-06 20:07:36 +0300

    subscribe fn and /subscriptions route

--- src/lib.rs
+++ src/lib.rs
@@ -13,10 +13,18 @@ async fn health_check() -> impl Responder {
 //     format!("Hello {}!", &name)
 // }
 
+async fn subscribe() -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
     // No .await here!
     Ok(server)
 }
