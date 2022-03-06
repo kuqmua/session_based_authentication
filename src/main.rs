0a915a2e17f34f37106434c256a1195fca580b31
kuqmua <serezhashmakov@outlook.com>
2022-03-06 17:22:32 +0300

    health_check

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,13 @@
 //     }
 // }
 
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+///////////////////////////////
+
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+async fn health_check(req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
 
 async fn greet(req: HttpRequest) -> impl Responder {
     let name = req.match_info().get("name").unwrap_or("World");
@@ -26,6 +32,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(greet))
+            .route("/health_check", web::get().to(health_check))
+            //order matters
             .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
