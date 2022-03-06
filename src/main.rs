e9278c2d29fc6aa921c90aaf4563e25504cecb97
kuqmua <serezhashmakov@outlook.com>
2022-03-06 16:58:21 +0300

    start tutorial from #3

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,34 @@
-pub mod routes {
-    pub mod home;
-    pub mod login;
-    // pub mod newsletters;
+// pub mod routes {
+//     pub mod home;
+//     pub mod login;
+//     // pub mod newsletters;
+// }
+// pub mod startup;
+
+// use crate::startup::run;
+
+// // #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
+// fn main() {
+//     if let Err(e) = run() {
+//         println!("run error {:#?}", e);
+//     }
+// }
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
 }
-pub mod startup;
 
-use crate::startup::run;
-
-// #[tokio::main(flavor = "multi_thread", worker_threads = 4)]
-fn main() {
-    if let Err(e) = run() {
-        println!("run error {:#?}", e);
-    }
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
