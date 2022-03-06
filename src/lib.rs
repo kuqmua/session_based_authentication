15396cb51102f06f359909eea368aa19203ead22
kuqmua <serezhashmakov@outlook.com>
2022-03-06 18:26:54 +0300

    comment out some routes

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,23 @@
 // pub mod authentication;
 
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, HttpResponse, HttpServer, Responder}; //HttpRequest,
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
+// async fn greet(req: HttpRequest) -> impl Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
 
 pub async fn run() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
+            // .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-            //order matters
-            .route("/{name}", web::get().to(greet))
+        //order matters
+        // .route("/{name}", web::get().to(greet))
     })
     .bind("127.0.0.1:8000")?
     .run()
