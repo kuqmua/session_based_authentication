7200348935d1fc60c4cad58976a4a29bf135c54a
kuqmua <serezhashmakov@outlook.com>
2022-03-07 11:57:24 +0300

    some tweaks

--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,9 +3,9 @@ set -x
 set -eo pipefail
 
 # Check if a custom user has been set, otherwise default to 'postgres'
-DB_USER=${POSTGRES_USER:=postgres}
+DB_USER=${POSTGRES_USER:=postgreslocalusername}
 # Check if a custom password has been set, otherwise default to 'password'
-DB_PASSWORD="${POSTGRES_PASSWORD:=password}"
+DB_PASSWORD="${POSTGRES_PASSWORD:=postgreslocalpassword}"
 # Check if a custom database name has been set, otherwise default to 'newsletter'
 DB_NAME="${POSTGRES_DB:=newsletter}"
 # Check if a custom port has been set, otherwise default to '5432'
