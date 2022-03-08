224235b01284b53d61f10ad7ce773be731b21487
kuqmua <serezhashmakov@outlook.com>
2022-03-08 12:36:35 +0300

    let request_id = Uuid::new_v4();

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,12 +11,17 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
+    let request_id = Uuid::new_v4();
     log::info!(
-        "Adding '{}' '{}' as a new subscriber.",
+        "request_id {} - Adding '{}' '{}' as a new subscriber.",
+        request_id,
         form.email,
         form.name
     );
-    log::info!("Saving new subscriber details in the database");
+    log::info!(
+        "request_id {} - Saving new subscriber details in the database",
+        request_id
+    );
     // `Result` has two variants: `Ok` and `Err`.
     // The first for successes, the second for failures.
     // We use a `match` statement to choose what to do based
@@ -36,11 +41,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("Saving new subscriber details in the database");
+            log::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            log::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
