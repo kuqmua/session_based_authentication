2349d014e1387dcf2e0cc988578a46733a0ae4b8
kuqmua <serezhashmakov@outlook.com>
2022-03-08 09:16:02 +0300

    pub async fn subscribe query

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,17 +1,32 @@
 use actix_web::{web, HttpResponse};
 use sqlx::PgConnection;
 
+use chrono::Utc;
+use uuid::Uuid;
+
 #[derive(serde::Deserialize)]
 pub struct FormData {
     email: String,
     name: String,
 }
 
-// Let's start simple: we always return a 200 OK
 pub async fn subscribe(
-    _form: web::Form<FormData>,
-    // Retrieving a connection from the application state!
-    _connection: web::Data<PgConnection>,
+    form: web::Form<FormData>,
+    connection: web::Data<PgConnection>,
 ) -> HttpResponse {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        form.email,
+        form.name,
+        Utc::now()
+    )
+    // We use `get_ref` to get an immutable reference to the `PgConnection`
+    // wrapped by `web::Data`.
+    .execute(connection.get_ref())
+    .await;
     HttpResponse::Ok().finish()
 }
