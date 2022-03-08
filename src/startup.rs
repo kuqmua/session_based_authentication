9ba5c7f492234dd39c0bdf4608abac89d0dcd4af
kuqmua <serezhashmakov@outlook.com>
2022-03-08 09:07:33 +0300

    web::Data::new(connection); inside run

--- src/startup.rs
+++ src/startup.rs
@@ -1,10 +1,6 @@
-use actix_web::web;
-use actix_web::App;
-use actix_web::HttpServer;
-
-use crate::routes::home::home;
-use crate::routes::login::login;
-use crate::routes::login::login_form;
+// use crate::routes::home::home;
+// use crate::routes::login::login;
+// use crate::routes::login::login_form;
 //
 // fn run(/* */) -> Result</* */> {
 //     let server = HttpServer::new(move || {
@@ -13,15 +9,37 @@ use crate::routes::login::login_form;
 //     })
 // }
 
-#[actix_web::main]
-pub async fn run() -> std::io::Result<()> {
-    HttpServer::new(|| {
+// #[actix_web::main]
+// pub async fn run() -> std::io::Result<()> {
+//     HttpServer::new(|| {
+//         App::new()
+//             .route("/login_form", web::get().to(login_form))
+//             .route("/login", web::post().to(login))
+//             .route("/", web::get().to(home))
+//     })
+//     .bind("127.0.0.1:8000")?
+//     .run()
+//     .await
+// }
+
+use crate::routes::{health_check, subscribe};
+use actix_web::dev::Server;
+use actix_web::{web, App, HttpServer};
+use sqlx::PgConnection;
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener, connection: PgConnection) -> Result<Server, std::io::Error> {
+    // Wrap the connection in a smart pointer
+    let connection = web::Data::new(connection);
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
-            .route("/login_form", web::get().to(login_form))
-            .route("/login", web::post().to(login))
-            .route("/", web::get().to(home))
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and attach it to the application state
+            .app_data(connection.clone())
     })
-    .bind("127.0.0.1:8000")?
-    .run()
-    .await
+    .listen(listener)?
+    .run();
+    Ok(server)
 }
