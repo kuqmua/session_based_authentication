92aacdd869ca0630ea3940679ddd129ab20b3a48
kuqmua <serezhashmakov@outlook.com>
2022-03-08 16:50:20 +0300

    telemetry module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
+#![allow(clippy::toplevel_ref_arg)]
 pub mod configuration;
 pub mod routes;
 pub mod startup;
+pub mod telemetry;
 
 // pub mod authentication;
 
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,12 @@
 use session_based_authentication::configuration::get_configuration;
 use session_based_authentication::startup::run;
+use session_based_authentication::telemetry::{get_subscriber, init_subscriber};
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::{subscriber::set_global_default, Subscriber};
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_log::LogTracer;
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-
-pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
-    let env_filter =
-        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
-    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
-    Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer)
-}
-
-/// Register a subscriber as global default to process span data.
-///
-/// It should only be called once!
-pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
-    LogTracer::init().expect("Failed to set logger");
-    set_global_default(subscriber).expect("Failed to set subscriber");
-}
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    let subscriber = get_subscriber("session_based_authentication".into(), "info".into());
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
