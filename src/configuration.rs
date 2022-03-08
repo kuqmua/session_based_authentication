283f8e0321033e24b5e851973c85ef50b9a14686
kuqmua <serezhashmakov@outlook.com>
2022-03-08 10:40:45 +0300

    DatabaseSettings connection_string_without_db

--- src/configuration.rs
+++ src/configuration.rs
@@ -22,6 +22,13 @@ impl DatabaseSettings {
         println!("url {}", f);
         f
     }
+
+    pub fn connection_string_without_db(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}",
+            self.username, self.password, self.host, self.port
+        )
+    }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
