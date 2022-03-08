dfed43354b03bcd816828b827ae65582fd5f50f3
kuqmua <serezhashmakov@outlook.com>
2022-03-08 09:09:45 +0300

    different pub async fn subscribe impl

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, HttpResponse};
+use sqlx::PgConnection;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -7,6 +8,10 @@ pub struct FormData {
 }
 
 // Let's start simple: we always return a 200 OK
-pub async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+pub async fn subscribe(
+    _form: web::Form<FormData>,
+    // Retrieving a connection from the application state!
+    _connection: web::Data<PgConnection>,
+) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
