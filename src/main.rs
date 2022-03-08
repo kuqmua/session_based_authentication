51a3c065d9608a1688c5e4ebf33907fd3024e486
kuqmua <serezhashmakov@outlook.com>
2022-03-08 09:07:44 +0300

    update main

--- src/main.rs
+++ src/main.rs
@@ -25,15 +25,31 @@ mod tests {
 
 // mod configuration;
 
+// use session_based_authentication::configuration::get_configuration;
+// use session_based_authentication::run;
+// use std::net::TcpListener;
+
+// #[tokio::main]
+// async fn main() -> std::io::Result<()> {
+//     let configuration = get_configuration().expect("Failed to read configuration.");
+//     let address = format!("127.0.0.1:{}", configuration.application_port);
+//     let listener = TcpListener::bind(address).expect("Failed to bind random port");
+//     // We retrieve the port assigned to us by the OS
+//     run(listener)?.await
+// }
+
 use session_based_authentication::configuration::get_configuration;
-use session_based_authentication::run;
+use session_based_authentication::startup::run;
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgConnection::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
-    let listener = TcpListener::bind(address).expect("Failed to bind random port");
-    // We retrieve the port assigned to us by the OS
-    run(listener)?.await
+    let listener = TcpListener::bind(address)?;
+    run(listener, connection)?.await
 }
