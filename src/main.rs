55d32e14816d3a8aeb480ee420e42811f22cb707
kuqmua <serezhashmakov@outlook.com>
2022-03-08 16:45:59 +0300

    get_subscriber and init_subscriber

--- src/main.rs
+++ src/main.rs
@@ -2,23 +2,33 @@ use session_based_authentication::configuration::get_configuration;
 use session_based_authentication::startup::run;
 use sqlx::postgres::PgPool;
 use std::net::TcpListener;
-use tracing::subscriber::set_global_default;
+use tracing::{subscriber::set_global_default, Subscriber};
 use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
 use tracing_log::LogTracer;
 use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or(EnvFilter::new("info"));
-    let formatting_layer =
-        BunyanFormattingLayer::new("session_based_authentication".into(), std::io::stdout);
-    let subscriber = Registry::default()
+pub fn get_subscriber(name: String, env_filter: String) -> impl Subscriber + Send + Sync {
+    let env_filter =
+        EnvFilter::try_from_default_env().unwrap_or_else(|_| EnvFilter::new(env_filter));
+    let formatting_layer = BunyanFormattingLayer::new(name, std::io::stdout);
+    Registry::default()
         .with(env_filter)
         .with(JsonStorageLayer)
-        .with(formatting_layer);
+        .with(formatting_layer)
+}
+
+/// Register a subscriber as global default to process span data.
+///
+/// It should only be called once!
+pub fn init_subscriber(subscriber: impl Subscriber + Send + Sync) {
+    LogTracer::init().expect("Failed to set logger");
     set_global_default(subscriber).expect("Failed to set subscriber");
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
