5742ae67313b44151cb9050718cc5b722c93e92b
kuqmua <serezhashmakov@outlook.com>
2022-03-11 20:46:41 +0300

    cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ use session_based_authentication::configuration::get_configuration;
 use session_based_authentication::startup::run;
 use session_based_authentication::telemetry::{get_subscriber, init_subscriber};
 // use sqlx::postgres::PgPool;
-use std::net::TcpListener;
 use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -17,13 +17,14 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("Failed to read configuration.");
     let connection_pool = PgPoolOptions::new()
-    .connect_timeout(std::time::Duration::from_secs(2)).connect_lazy(&configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to Postgres.");
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret())
+        .expect("Failed to connect to Postgres.");
 
     let address = format!(
-                "{}:{}",
-                configuration.application.host, configuration.application.port
-            );
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
