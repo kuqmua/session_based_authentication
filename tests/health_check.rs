ad67385bac5c922cfefacb1013356038ed0e1a92
kuqmua <serezhashmakov@outlook.com>
2022-03-12 13:04:15 +0300

    return old subscribe_returns_a_200_for_valid_form_data

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -95,50 +95,50 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     let app = spawn_app().await;
     let client = reqwest::Client::new();
     //
-    let test_cases = vec![
-        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
-        ("name=Ursula&email=", "empty email"),
-        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
-    ];
-    //
-    for (body, description) in test_cases {
-        // Act
-        let response = client
-            .post(&format!("{}/subscriptions", &app.address))
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .body(body)
-            .send()
-            .await
-            .expect("Failed to execute request.");
+    // let test_cases = vec![
+    //     ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+    //     ("name=Ursula&email=", "empty email"),
+    //     ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    // ];
+    // //
+    // for (body, description) in test_cases {
+    //     // Act
+    //     let response = client
+    //         .post(&format!("{}/subscriptions", &app.address))
+    //         .header("Content-Type", "application/x-www-form-urlencoded")
+    //         .body(body)
+    //         .send()
+    //         .await
+    //         .expect("Failed to execute request.");
 
-        // Assert
-        assert_eq!(
-            200,
-            response.status().as_u16(),
-            "The API did not return a 200 OK when the payload was {}.",
-            description
-        );
-    }
+    //     // Assert
+    //     assert_eq!(
+    //         200,
+    //         response.status().as_u16(),
+    //         "The API did not return a 200 OK when the payload was {}.",
+    //         description
+    //     );
+    // }
     // // Act
-    // let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    // let response = client
-    //     .post(&format!("{}/subscriptions", &app.address))
-    //     .header("Content-Type", "application/x-www-form-urlencoded")
-    //     .body(body)
-    //     .send()
-    //     .await
-    //     .expect("Failed to execute request.");
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app.address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
 
-    // // Assert
-    // assert_eq!(200, response.status().as_u16());
+    // Assert
+    assert_eq!(200, response.status().as_u16());
 
-    // let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-    //     .fetch_one(&app.db_pool)
-    //     .await
-    //     .expect("Failed to fetch saved subscription.");
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&app.db_pool)
+        .await
+        .expect("Failed to fetch saved subscription.");
 
-    // assert_eq!(saved.email, "ursula_le_guin@gmail.com");
-    // assert_eq!(saved.name, "le guin");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
