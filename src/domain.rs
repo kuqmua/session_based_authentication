b1873fc31985215dae9f9d34a05835671868b15a
kuqmua <serezhashmakov@outlook.com>
2022-03-12 12:04:23 +0300

    better but still not good error handling

--- src/domain.rs
+++ src/domain.rs
@@ -46,7 +46,7 @@ impl SubscriberName {
         let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
 
         if is_empty_or_whitespace || is_too_long || contains_forbidden_characters {
-            panic!("{} is not a valid subscriber name.", s)
+            Err(format!("{} is not a valid subscriber name.", s))
         } else {
             Ok(Self(s))
         }
