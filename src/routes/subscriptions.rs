4e2f16c824fbc3f8348217abac8c5e38c5709c2b
kuqmua <serezhashmakov@outlook.com>
2022-03-12 14:14:59 +0300

    pub email: SubscriberEmail,

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -28,15 +28,12 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
         // Return early if the name is invalid, with a 400
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let subs_email = match SubscriberEmail::parse(form.0.email) {
+    let email = match SubscriberEmail::parse(form.0.email) {
         Ok(email) => email,
         // Return early if the email is invalid, with a 400
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
-    let new_subscriber = NewSubscriber {
-        email: subs_email.as_ref().to_string(),
-        name,
-    };
+    let new_subscriber = NewSubscriber { email, name };
     match insert_subscriber(&pool, &new_subscriber).await {
         Ok(_) => HttpResponse::Ok().finish(),
         Err(_) => HttpResponse::InternalServerError().finish(),
@@ -83,7 +80,7 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        new_subscriber.email,
+        new_subscriber.email.as_ref(),
         new_subscriber.name.as_ref(),
         Utc::now()
     )
