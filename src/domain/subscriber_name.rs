71741463af1df18463dee9ee8fe5285c0e037f4a
kuqmua <serezhashmakov@outlook.com>
2022-03-12 12:45:31 +0300

    domain templates

---
+++ src/domain/subscriber_name.rs
@@ -0,0 +1,17 @@
+use unicode_segmentation::UnicodeSegmentation;
+
+#[derive(Debug)]
+pub struct SubscriberName(String);
+
+impl SubscriberName {
+    // [...]
+}
+
+impl AsRef<str> for SubscriberName {
+    // [...]
+}
+
+#[cfg(test)]
+mod tests {
+    // [...]
+}
