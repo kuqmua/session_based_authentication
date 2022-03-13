7e11e2b0bc54d3e756c62a4ff95dd1ae89d1e15e
kuqmua <serezhashmakov@outlook.com>
2022-03-13 10:01:12 +0300

    struct SendEmailRequest

--- src/email_client.rs
+++ src/email_client.rs
@@ -7,6 +7,14 @@ pub struct EmailClient {
     sender: SubscriberEmail,
 }
 
+struct SendEmailRequest {
+    from: String,
+    to: String,
+    subject: String,
+    html_body: String,
+    text_body: String,
+}
+
 impl EmailClient {
     pub fn new(base_url: String, sender: SubscriberEmail) -> Self {
         Self {
@@ -23,6 +31,13 @@ impl EmailClient {
         text_content: &str,
     ) -> Result<(), String> {
         let url = format!("{}/email", self.base_url);
+        let request_body = SendEmailRequest {
+            from: self.sender.as_ref().to_owned(),
+            to: recipient.as_ref().to_owned(),
+            subject: subject.to_owned(),
+            html_body: html_content.to_owned(),
+            text_body: text_content.to_owned(),
+        };
         let builder = self.http_client.post(&url);
         Ok(())
     }
