0e7b3089db740c3af2a9ee795955a5c5e960e54d
kuqmua <serezhashmakov@outlook.com>
2022-03-15 21:18:05 +0300

    transaction.commit()

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -72,6 +72,9 @@ pub async fn subscribe(
     {
         return HttpResponse::InternalServerError().finish();
     }
+    if transaction.commit().await.is_err() {
+        return HttpResponse::InternalServerError().finish();
+    }
     if send_confirmation_email(
         &email_client, 
         new_subscriber, 
