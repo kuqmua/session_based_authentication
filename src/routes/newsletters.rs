1fbc260b2b2da8a3c8f7053949d892adaf7ff4a5
kuqmua <serezhashmakov@outlook.com>
2022-03-19 13:44:47 +0300

    tracing for publish_newsletter

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -130,7 +130,11 @@ struct Credentials {
     password: Secret<String>,
 }
 
-// Dummy implementation
+#[tracing::instrument(
+    name = "Publish a newsletter issue",
+    skip(body, pool, email_client, request),
+    fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
+)]
 pub async fn publish_newsletter(
     body: web::Json<BodyData>,
     pool: web::Data<PgPool>,
@@ -138,7 +142,9 @@ pub async fn publish_newsletter(
     request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
     let credentials = basic_authentication(request.headers()).map_err(PublishError::AuthError)?;
+    tracing::Span::current().record("username", &tracing::field::display(&credentials.username));
     let user_id = validate_credentials(credentials, &pool).await?;
+    tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
