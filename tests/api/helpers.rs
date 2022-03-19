bf8948108161d17263bb602406565af8f8f8f860
kuqmua <serezhashmakov@outlook.com>
2022-03-19 13:07:45 +0300

    impl TestApp async fn post_newsletters

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -39,6 +39,17 @@ pub struct TestApp {
 }
 
 impl TestApp {
+    pub async fn post_newsletters(&self, body: serde_json::Value) -> reqwest::Response {
+        reqwest::Client::new()
+            .post(&format!("{}/newsletters", &self.address))
+            // Random credentials!
+            // `reqwest` does all the encoding/formatting heavy-lifting for us.
+            .basic_auth(Uuid::new_v4().to_string(), Some(Uuid::new_v4().to_string()))
+            .json(&body)
+            .send()
+            .await
+            .expect("Failed to execute request.")
+    }
     pub async fn post_subscriptions(&self, body: String) -> reqwest::Response {
         reqwest::Client::new()
             .post(&format!("{}/subscriptions", &self.address))
