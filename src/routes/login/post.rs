e845ceebe1c1c3f3a5297e96c327ffdb129b1ec8
kuqmua <serezhashmakov@outlook.com>
2022-03-20 17:38:30 +0300

    impl ResponseError for LoginError {     fn error_response

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,4 +1,5 @@
 use crate::authentication::{validate_credentials, Credentials};
+use actix_web::http::header::ContentType;
 use actix_web::http::header::LOCATION;
 use actix_web::web;
 use actix_web::HttpResponse;
@@ -56,6 +57,40 @@ impl std::fmt::Debug for LoginError {
 }
 
 impl ResponseError for LoginError {
+    fn error_response(&self) -> HttpResponse {
+        HttpResponse::build(self.status_code())
+            .content_type(ContentType::html())
+            .body(format!(
+                r#"<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <title>Login</title>
+</head>
+<body>
+    <p><i>{}</i></p>
+    <form action="/login" method="post">
+        <label>Username
+            <input
+                type="text"
+                placeholder="Enter Username"
+                name="username"
+            >
+        </label>
+        <label>Password
+            <input
+                type="password"
+                placeholder="Enter Password"
+                name="password"
+            >
+        </label>
+        <button type="submit">Login</button>
+    </form>
+</body>
+</html>"#,
+                self
+            ))
+    }
     fn status_code(&self) -> StatusCode {
         match self {
             LoginError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
