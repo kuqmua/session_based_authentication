12c82464dbc9d24c19e935c90480ca5f6690275c
kuqmua <serezhashmakov@outlook.com>
2022-03-26 13:28:58 +0300

    impl QueryParams fn verify

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -2,12 +2,25 @@ use crate::startup::HmacSecret;
 use actix_web::http::header::ContentType;
 use actix_web::{web, HttpResponse};
 
+use hmac::{Hmac, Mac};
+use secrecy::ExposeSecret;
+
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
     error: String,
     tag: String,
 }
-
+impl QueryParams {
+    fn verify(self, secret: &HmacSecret) -> Result<String, anyhow::Error> {
+        let tag = hex::decode(self.tag)?;
+        let query_string = format!("error={}", urlencoding::Encoded::new(&self.error));
+        let mut mac =
+            Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes()).unwrap();
+        mac.update(query_string.as_bytes());
+        mac.verify_slice(&tag)?;
+        Ok(self.error)
+    }
+}
 pub async fn login_form(
     query: Option<web::Query<QueryParams>>,
     secret: web::Data<HmacSecret>,
