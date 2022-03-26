dd57047bff4c0c885597e94938ef2ab8305582e2
kuqmua <serezhashmakov@outlook.com>
2022-03-26 18:48:46 +0300

    pub async fn login_form(request: HttpRequest)

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -1,8 +1,7 @@
 use crate::startup::HmacSecret;
 use actix_web::http::header::ContentType;
-use actix_web::{web, HttpResponse};
-
-use hmac::{Hmac, Mac};
+use actix_web::{web, HttpRequest, HttpResponse};
+use hmac::{Hmac, Mac}; //, NewMac
 use secrecy::ExposeSecret;
 
 #[derive(serde::Deserialize)]
@@ -21,25 +20,12 @@ impl QueryParams {
         Ok(self.error)
     }
 }
-pub async fn login_form(
-    query: Option<web::Query<QueryParams>>,
-    secret: web::Data<HmacSecret>,
-) -> HttpResponse {
-    let error_html = match query {
+pub async fn login_form(request: HttpRequest) -> HttpResponse {
+    let error_html = match request.cookie("_flash") {
         None => "".into(),
-        Some(query) => match query.0.verify(&secret) {
-            Ok(error) => {
-                format!("<p><i>{}</i></p>", htmlescape::encode_minimal(&error))
-            }
-            Err(e) => {
-                tracing::warn!(
-                    error.message = %e,
-                    error.cause_chain = ?e,
-                    "Failed to verify query parameters using the HMAC tag"
-                );
-                "".into()
-            }
-        },
+        Some(cookie) => {
+            format!("<p><i>{}</i></p>", cookie.value())
+        }
     };
     HttpResponse::Ok()
         .content_type(ContentType::html())
