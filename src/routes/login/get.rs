4b6091339c2fbb188a101ac5f90dae36316f85f7
kuqmua <serezhashmakov@outlook.com>
2022-03-27 16:03:26 +0300

    login_form(flash_messages: IncomingFlashMessages)

--- src/routes/login/get.rs
+++ src/routes/login/get.rs
@@ -4,6 +4,8 @@ use actix_web::http::header::ContentType;
 use actix_web::{web, HttpRequest, HttpResponse};
 use hmac::{Hmac, Mac}; //, NewMac
 use secrecy::ExposeSecret;
+use actix_web_flash_messages::{IncomingFlashMessages, Level};
+use std::fmt::Write;
 
 #[derive(serde::Deserialize)]
 pub struct QueryParams {
@@ -21,16 +23,16 @@ impl QueryParams {
         Ok(self.error)
     }
 }
-pub async fn login_form(request: HttpRequest) -> HttpResponse {
-    let error_html = match request.cookie("_flash") {
-        None => "".into(),
-        Some(cookie) => {
-            format!("<p><i>{}</i></p>", cookie.value())
-        }
-    };
-    let mut response = HttpResponse::Ok()
+pub async fn login_form(flash_messages: IncomingFlashMessages) -> HttpResponse {
+    //
+    let mut error_html = String::new();
+    for m in flash_messages.iter().filter(|m| m.level() == Level::Error) {
+        writeln!(error_html, "<p><i>{}</i></p>", m.content()).unwrap();
+    }
+    // let mut response = 
+    HttpResponse::Ok()
         .content_type(ContentType::html())
-        .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
+        // .cookie(Cookie::build("_flash", "").max_age(Duration::ZERO).finish())
         .body(format!(
             r#"<!DOCTYPE html>
 <html lang="en">
@@ -59,9 +61,10 @@ pub async fn login_form(request: HttpRequest) -> HttpResponse {
     </form>
 </body>
 </html>"#,
-        ));
-    response
-        .add_removal_cookie(&Cookie::new("_flash", ""))
-        .unwrap();
-    response
+        ))
+    //     ;
+    // response
+    //     .add_removal_cookie(&Cookie::new("_flash", ""))
+    //     .unwrap();
+    // response
 }
