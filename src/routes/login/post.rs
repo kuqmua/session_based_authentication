4ebd288f5d23b5983825b603c685c9e16eb0f4f2
kuqmua <serezhashmakov@outlook.com>
2022-03-27 18:45:55 +0300

    session.insert("user_id", user_id);

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -18,6 +18,7 @@ use actix_web_flash_messages::FlashMessage;
 // use hmac::{Hmac, Mac};
 
 use actix_web::error::InternalError;
+use actix_session::Session;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -26,12 +27,13 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    skip(form, pool),
+    skip(form, pool, session),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
+    session: Session,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -41,6 +43,7 @@ pub async fn login(
     match validate_credentials(credentials, &pool).await {
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
+            session.insert("user_id", user_id);
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
                 .finish())
