f9aa3f53bf44938a31ca5f9d40965b44ef24b386
kuqmua <serezhashmakov@outlook.com>
2022-03-28 19:18:30 +0300

    impl FromRequest for TypedSession

--- src/session_state.rs
+++ src/session_state.rs
@@ -1,5 +1,9 @@
 use actix_session::Session;
 use uuid::Uuid;
+use actix_session::SessionExt;
+use actix_web::dev::Payload;
+use actix_web::{FromRequest, HttpRequest};
+use std::future::{Ready, ready};
 
 pub struct TypedSession(Session);
 
@@ -16,5 +20,23 @@ impl TypedSession {
 
     pub fn get_user_id(&self) -> Result<Option<Uuid>, serde_json::Error> {
         self.0.get(Self::USER_ID_KEY)
+    }
+}
+
+impl FromRequest for TypedSession {
+    // This is a complicated way of saying 
+    // "We return the same error returned by the 
+    // implementation of `FromRequest` for `Session`".
+    type Error = <Session as FromRequest>::Error;
+    // Rust does not yet support the `async` syntax in traits.
+    // From request expects a `Future` as return type to allow for extractors
+    // that need to perform asynchronous operations (e.g. a HTTP call)
+    // We do not have a `Future`, because we don't perform any I/O,
+    // so we wrap `TypedSession` into `Ready` to convert it into a `Future` that
+    // resolves to the wrapped value the first time it's polled by the executor.
+    type Future = Ready<Result<TypedSession, Self::Error>>;
+
+    fn from_request(req: &HttpRequest, _payload: &mut Payload) -> Self::Future {
+        ready(Ok(TypedSession(req.get_session())))
     }
 }
