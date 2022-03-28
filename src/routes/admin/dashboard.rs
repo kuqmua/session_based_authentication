184f339392648e8c18053decc1ea9f10c5f794e0
kuqmua <serezhashmakov@outlook.com>
2022-03-28 18:33:47 +0300

    async fn get_username

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,6 +1,10 @@
 use actix_session::Session;
 use actix_web::{web, HttpResponse};
 use uuid::Uuid;
+use actix_web::http::header::ContentType;
+// use actix_web::web;
+use anyhow::Context;
+use sqlx::PgPool;
 
 // Return an opaque 500 while preserving the error's root cause for logging.
 fn e500<T>(e: T) -> actix_web::Error 
@@ -22,4 +26,23 @@ pub async fn admin_dashboard(
         todo!()
     };
     Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(name = "Get username", skip(pool))]
+async fn get_username(
+    user_id: Uuid, 
+    pool: &PgPool
+) -> Result<String, anyhow::Error> {
+    let row = sqlx::query!(
+        r#"
+        SELECT username
+        FROM users
+        WHERE user_id = $1
+        "#,
+        user_id,
+    )
+    .fetch_one(pool)
+    .await
+    .context("Failed to perform a query to retrieve a username.")?;
+    Ok(row.username)
 }
