fac9c001af46d6d612ba0d850ce04ecaa5fee842
kuqmua <serezhashmakov@outlook.com>
2022-03-28 20:28:23 +0300

    change_password and change_password_form changes

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,5 +1,7 @@
 use actix_web::{HttpResponse, web};
 use secrecy::Secret;
+use crate::session_state::TypedSession;
+use crate::utils::{e500, see_other};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -10,6 +12,10 @@ pub struct FormData {
 
 pub async fn change_password(
     form: web::Form<FormData>,
+    session: TypedSession,
 ) -> Result<HttpResponse, actix_web::Error> {
+    if session.get_user_id().map_err(e500)?.is_none() {
+        return Ok(see_other("/login"));
+    };
     todo!()
 }
