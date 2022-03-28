9097df9c8996a3ec426ebecb79ba0348f8f65799
kuqmua <serezhashmakov@outlook.com>
2022-03-28 19:22:53 +0300

    use crate::session_state::TypedSession;

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,5 +1,5 @@
 use crate::authentication::{validate_credentials, Credentials};
-use actix_web::cookie::Cookie;
+// use actix_web::cookie::Cookie;
 // use actix_web::http::header::ContentType;
 use actix_web::http::header::LOCATION;
 use actix_web::web;
@@ -18,7 +18,8 @@ use actix_web_flash_messages::FlashMessage;
 // use hmac::{Hmac, Mac};
 
 use actix_web::error::InternalError;
-use actix_session::Session;
+// use actix_session::Session;
+use crate::session_state::TypedSession;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -33,7 +34,7 @@ pub struct FormData {
 pub async fn login(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
-    session: Session,
+    session: TypedSession,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -44,7 +45,7 @@ pub async fn login(
         Ok(user_id) => {
             tracing::Span::current().record("user_id", &tracing::field::display(&user_id));
             session.renew();
-            session.insert("user_id", user_id).map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
+            session.insert_user_id(user_id).map_err(|e| login_redirect(LoginError::UnexpectedError(e.into())))?;
             Ok(HttpResponse::SeeOther()
                 .insert_header((LOCATION, "/admin/dashboard"))
                 .finish())
