cfbba85a3b00d3721bdb819298ed0d442f6f0d92
kuqmua <serezhashmakov@outlook.com>
2022-03-28 18:31:19 +0300

    admin_dashboard e500

--- src/routes/admin/dashboard.rs
+++ src/routes/admin/dashboard.rs
@@ -1,5 +1,25 @@
-use actix_web::HttpResponse;
+use actix_session::Session;
+use actix_web::{web, HttpResponse};
+use uuid::Uuid;
 
-pub async fn admin_dashboard() -> HttpResponse {
-    HttpResponse::Ok().finish()
+// Return an opaque 500 while preserving the error's root cause for logging.
+fn e500<T>(e: T) -> actix_web::Error 
+where
+    T: std::fmt::Debug + std::fmt::Display + 'static
+{
+    actix_web::error::ErrorInternalServerError(e)
+}
+
+pub async fn admin_dashboard(
+    session: Session
+) -> Result<HttpResponse, actix_web::Error> {
+    let _username = if let Some(user_id) = session
+        .get::<Uuid>("user_id")
+        .map_err(e500)?
+    {
+        todo!()
+    } else {
+        todo!()
+    };
+    Ok(HttpResponse::Ok().finish())
 }
