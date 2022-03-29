04c0214c4e497c85e0bd85174e1257b32d3bb847
kuqmua <serezhashmakov@outlook.com>
2022-03-29 21:25:28 +0300

    reject_anonymous_users upd

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -1,10 +1,16 @@
 use actix_web_lab::middleware::Next;
 use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
+use actix_web::FromRequest;
+use crate::session_state::TypedSession;
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
     next: Next<impl MessageBody>,
 ) -> Result<ServiceResponse<impl MessageBody>, actix_web::Error> {
+    let session = {
+        let (http_request, payload) = req.parts_mut();
+        TypedSession::from_request(http_request, payload).await
+    }?;
     todo!()
 }
