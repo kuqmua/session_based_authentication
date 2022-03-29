78596de7eced736232de3f5746b5676f9722110d
kuqmua <serezhashmakov@outlook.com>
2022-03-29 21:28:29 +0300

    reject_anonymous_users middleware

--- src/authentication/middleware.rs
+++ src/authentication/middleware.rs
@@ -3,6 +3,8 @@ use actix_web::body::MessageBody;
 use actix_web::dev::{ServiceRequest, ServiceResponse};
 use actix_web::FromRequest;
 use crate::session_state::TypedSession;
+use actix_web::error::InternalError;
+use crate::utils::{e500, see_other};
 
 pub async fn reject_anonymous_users(
     mut req: ServiceRequest,
@@ -12,5 +14,12 @@ pub async fn reject_anonymous_users(
         let (http_request, payload) = req.parts_mut();
         TypedSession::from_request(http_request, payload).await
     }?;
-    todo!()
+    match session.get_user_id().map_err(e500)? {
+        Some(_) => next.call(req).await,
+        None => {
+            let response = see_other("/login");
+            let e = anyhow::anyhow!("The user has not logged in");
+            Err(InternalError::from_response(e, response).into())
+        }
+    }
 }
