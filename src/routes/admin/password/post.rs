36e3493850347b0d9535eb0c8d4a0839122206bd
kuqmua <serezhashmakov@outlook.com>
2022-03-29 19:06:05 +0300

    change_password pool

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -4,6 +4,8 @@ use crate::session_state::TypedSession;
 use crate::utils::{e500, see_other};
 use secrecy::ExposeSecret;
 use actix_web_flash_messages::FlashMessage;
+use crate::routes::admin::dashboard::get_username;
+use sqlx::PgPool;
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -15,10 +17,13 @@ pub struct FormData {
 pub async fn change_password(
     form: web::Form<FormData>,
     session: TypedSession,
+    pool: web::Data<PgPool>,
 ) -> Result<HttpResponse, actix_web::Error> {
-    if session.get_user_id().map_err(e500)?.is_none() {
+    let user_id = session.get_user_id().map_err(e500)?;
+    if user_id.is_none() {
         return Ok(see_other("/login"));
     };
+    let user_id = user_id.unwrap();
     if form.new_password.expose_secret() != form.new_password_check.expose_secret() {
         FlashMessage::error(
             "You entered two different new passwords - the field values must match.",
@@ -26,5 +31,6 @@ pub async fn change_password(
         .send();
         return Ok(see_other("/admin/password"));
     }
+    let username = get_username(user_id, &pool).await.map_err(e500)?;
     todo!()
 }
