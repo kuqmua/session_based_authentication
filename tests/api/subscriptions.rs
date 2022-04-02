30ae20af4acfb9ca24565bf52fc6c8965552f7ed
kuqmua <serezhashmakov@outlook.com>
2022-04-02 18:18:00 +0300

    subscriptions test mod cleaning

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -4,44 +4,31 @@ use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-
-    // Act
     let response = app.post_subscriptions(body.into()).await;
-
-    // Assert
     assert_eq!(200, response.status().as_u16());
 }
 
 #[tokio::test]
 async fn subscribe_persists_the_new_subscriber() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-
-    // Act
     app.post_subscriptions(body.into()).await;
-
-    // Assert
     let saved = sqlx::query!("SELECT email, name, status FROM subscriptions",)
         .fetch_one(&app.db_pool)
         .await
         .expect("Failed to fetch saved subscription.");
-
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
     assert_eq!(saved.status, "pending_confirmation");
@@ -49,24 +36,17 @@ async fn subscribe_persists_the_new_subscriber() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
-    // Arrange
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
         ("", "missing both name and email"),
     ];
-
     for (invalid_body, error_message) in test_cases {
-        // Act
         let response = app.post_subscriptions(invalid_body.into()).await;
-
-        // Assert
         assert_eq!(
             400,
             response.status().as_u16(),
-            // Additional customised error message on test failure
             "The API did not fail with 400 Bad Request when the payload was {}.",
             error_message
         );
@@ -75,20 +55,14 @@ async fn subscribe_returns_a_400_when_data_is_missing() {
 
 #[tokio::test]
 async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
-    // Arrange
     let app = spawn_app().await;
-    let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
         ("name=Ursula&email=", "empty email"),
         ("name=Ursula&email=definitely-not-an-email", "invalid email"),
     ];
-
     for (body, description) in test_cases {
-        // Act
         let response = app.post_subscriptions(body.into()).await;
-
-        // Assert
         assert_eq!(
             400,
             response.status().as_u16(),
@@ -100,27 +74,17 @@ async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
 
 #[tokio::test]
 async fn subscribe_sends_a_confirmation_email_for_valid_data() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .expect(1)
         .mount(&app.email_server)
         .await;
-
-    // Act
     app.post_subscriptions(body.into()).await;
-
-    // Assert
-    // Mock asserts on drop
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    // Parse the body as JSON, starting from raw bytes
     let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
-
-    // Extract the link from one of the request fields.
     let get_link = |s: &str| {
         let links: Vec<_> = linkify::LinkFinder::new()
             .links(s)
@@ -130,49 +94,34 @@ async fn subscribe_sends_a_confirmation_email_for_valid_data() {
         links[0].as_str().to_owned()
     };
 
-    let html_link = get_link(&body["HtmlBody"].as_str().unwrap());
-    let text_link = get_link(&body["TextBody"].as_str().unwrap());
-    // The two links should be identical
+    let html_link = get_link(body["HtmlBody"].as_str().unwrap());
+    let text_link = get_link(body["TextBody"].as_str().unwrap());
     assert_eq!(html_link, text_link);
 }
 
 #[tokio::test]
 async fn subscribe_sends_a_confirmation_email_with_a_link() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-
     Mock::given(path("/email"))
         .and(method("POST"))
         .respond_with(ResponseTemplate::new(200))
         .mount(&app.email_server)
         .await;
-
-    // Act
     app.post_subscriptions(body.into()).await;
-
-    // Assert
     let email_request = &app.email_server.received_requests().await.unwrap()[0];
-    let confirmation_links = app.get_confirmation_links(&email_request);
-
-    // The two links should be identical
+    let confirmation_links = app.get_confirmation_links(email_request);
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
 
 #[tokio::test]
 async fn subscribe_fails_if_there_is_a_fatal_database_error() {
-    // Arrange
     let app = spawn_app().await;
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-    // Sabotage the database
     sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;",)
         .execute(&app.db_pool)
         .await
         .unwrap();
-
-    // Act
     let response = app.post_subscriptions(body.into()).await;
-
-    // Assert
     assert_eq!(response.status().as_u16(), 500);
 }
