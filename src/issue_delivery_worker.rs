65a93b393e99bfe8f120dd6d9df5e018021a3f40
kuqmua <serezhashmakov@outlook.com>
2022-04-02 12:05:31 +0300

    run_worker_until_stopped upd

--- src/issue_delivery_worker.rs
+++ src/issue_delivery_worker.rs
@@ -155,16 +155,20 @@ async fn worker_loop(pool: PgPool, email_client: EmailClient) -> Result<(), anyh
 
 pub async fn run_worker_until_stopped(configuration: Settings) -> Result<(), anyhow::Error> {
     let connection_pool = get_connection_pool(&configuration.database);
-    let sender_email = configuration
-        .email_client
-        .sender()
-        .expect("Invalid sender email address.");
-    let timeout = configuration.email_client.timeout();
-    let email_client = EmailClient::new(
-        configuration.email_client.base_url,
-        sender_email,
-        configuration.email_client.authorization_token,
-        timeout,
-    );
+    // Use helper function!
+    let email_client = configuration.email_client.client();
     worker_loop(connection_pool, email_client).await
+    // let connection_pool = get_connection_pool(&configuration.database);
+    // let sender_email = configuration
+    //     .email_client
+    //     .sender()
+    //     .expect("Invalid sender email address.");
+    // let timeout = configuration.email_client.timeout();
+    // let email_client = EmailClient::new(
+    //     configuration.email_client.base_url,
+    //     sender_email,
+    //     configuration.email_client.authorization_token,
+    //     timeout,
+    // );
+    // worker_loop(connection_pool, email_client).await
 }
