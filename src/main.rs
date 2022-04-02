88a755e49e81cd81f7d1f2e939eef2f1942f10aa
kuqmua <serezhashmakov@outlook.com>
2022-04-02 11:46:26 +0300

    tokio::select! inside main

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use session_based_authentication::configuration::get_configuration;
+use session_based_authentication::issue_delivery_worker::run_worker_until_stopped;
 use session_based_authentication::startup::Application;
 use session_based_authentication::telemetry::{get_subscriber, init_subscriber};
 
@@ -12,7 +13,13 @@ async fn main() -> anyhow::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration).await?;
-    application.run_until_stopped().await?;
+    let application = Application::build(configuration.clone())
+        .await?
+        .run_until_stopped();
+    let worker = run_worker_until_stopped(configuration);
+    tokio::select! {
+        _ = application => {},
+        _ = worker => {},
+    };
     Ok(())
 }
