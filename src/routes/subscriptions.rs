be8c453c57eebd3de9f86c74fadac52ae8e2ab6b
kuqmua <serezhashmakov@outlook.com>
2022-04-02 18:48:26 +0300

    subs cleaning

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -19,7 +19,6 @@ pub struct FormData {
 
 impl TryFrom<FormData> for NewSubscriber {
     type Error = String;
-
     fn try_from(value: FormData) -> Result<Self, Self::Error> {
         let name = SubscriberName::parse(value.name)?;
         let email = SubscriberEmail::parse(value.email)?;
