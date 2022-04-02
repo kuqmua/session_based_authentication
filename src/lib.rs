0cf852a1ef50bfcc5272db11ad04500b503eee23
kuqmua <serezhashmakov@outlook.com>
2022-04-02 11:21:39 +0300

    issue_delivery_worker mod

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,13 @@ pub mod authentication;
 pub mod configuration;
 pub mod domain;
 pub mod email_client;
+pub mod idempotency;
+pub mod issue_delivery_worker;
 pub mod routes;
+pub mod session_state;
 pub mod startup;
 pub mod telemetry;
-pub mod session_state;
 pub mod utils;
-pub mod idempotency;
 
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpResponse, HttpServer}; //HttpRequest, Responder
