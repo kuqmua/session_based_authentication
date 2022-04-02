549c95128639f3d836aca2418b674a83d8fd1cfb
kuqmua <serezhashmakov@outlook.com>
2022-04-02 18:44:38 +0300

    cleaning lib .rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-#![allow(clippy::toplevel_ref_arg)]
 pub mod authentication;
 pub mod configuration;
 pub mod domain;
@@ -10,32 +9,3 @@ pub mod session_state;
 pub mod startup;
 pub mod telemetry;
 pub mod utils;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpResponse, HttpServer};
-use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
