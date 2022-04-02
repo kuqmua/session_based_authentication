fd67c8eb678c75c90265f280d08f6beabd41999e
kuqmua <serezhashmakov@outlook.com>
2022-04-02 11:49:49 +0300

    tokio::spawn inside main

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,12 @@ async fn main() -> anyhow::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let application = Application::build(configuration.clone())
-        .await?
-        .run_until_stopped();
-    let worker = run_worker_until_stopped(configuration);
+    let application = Application::build(configuration.clone()).await?;
+    let application_task = tokio::spawn(application.run_until_stopped());
+    let worker_task = tokio::spawn(run_worker_until_stopped(configuration));
     tokio::select! {
-        _ = application => {},
-        _ = worker => {},
+        _ = application_task => {},
+        _ = worker_task => {},
     };
     Ok(())
 }
